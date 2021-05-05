d5478d5c86ad1f22e139d4b72a527082ea792f9d
Hans Czajkowski Jørgensen <hans.c.jorgensen@gmail.com>
2021-05-05 20:09:03 +0200

    Add fallback for thumbnail

--- acf_block_template.php
+++ acf_block_template.php
@@ -35,10 +35,15 @@ if( !empty($block['className']) ) {
 
                 $json = file_get_contents('https://www.googleapis.com/youtube/v3/videos?id=' . $video_id . '&key=' . $api_key . '&part=snippet');
                 $data = json_decode($json, true);
+                $thumbnail_size = 'standard';
+                if (!array_key_exists($thumbnail_size, $data['items'][0]['snippet']['thumbnails'])) {
+                    $thumbnail_size = 'high';
+                }
+
             ?>
             <a class="yt-video" href="<?php echo $video_url; ?>">
                 <div class="yt-video-thumbnail">
-                    <img src="<?php echo $data['items'][0]['snippet']['thumbnails']['standard']['url'] ?>">
+                    <img src="<?php echo $data['items'][0]['snippet']['thumbnails'][$thumbnail_size]['url'] ?>">
                     <svg class="yt-video-thumbnail-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 311.69 311.69"><path d="M155.84,0A155.85,155.85,0,1,0,311.69,155.84,155.84,155.84,0,0,0,155.84,0Zm0,296.42A140.58,140.58,0,1,1,296.42,155.84,140.58,140.58,0,0,1,155.84,296.42Z"></path><polygon points="218.79 155.84 119.22 94.34 119.22 217.34 218.79 155.84"></polygon></svg>
                 </div>
                 <p class="yt-video-title"> <?php echo $data['items'][0]['snippet']['title'] ?></p>
